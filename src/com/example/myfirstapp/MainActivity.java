d666b07fd59f96043821e49110bb163b1a0b9836
SnehaKK <kulsneha04@gmail.com>
2014-02-14 17:08:05 -0800

    Adding Code for stock ticker
    Adding Stock ticker code via Yahoo.finance api

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -37,9 +37,56 @@ public class MainActivity extends Activity {
 	 * To check the stock entered by the user and provide the value
 	 */
 	public void checkStock(View view){
-		Intent intent =new Intent(this, MainActivity.class);
-		EditText enteredText= (EditText) findViewById(R.id.edit_message);
-		String stockName= enteredText.getText().toString();
 		
+		EditText enteredText = (EditText) findViewById(R.id.edit_message);
+		String stockName = enteredText.getText().toString();
+		new getStockPrice().execute(stockName);
+	}
+    
+	public class getStockPrice extends AsyncTask<String, Void, String> {
+        
+		/**
+		 * The system calls this to perform work in a worker thread and delivers
+		 * it the parameters given to AsyncTask.execute()
+		 */
+		protected String doInBackground(String... params) {
+            
+			// Get the stock price and display it to the user.
+            
+			// URL to retrieve the stock value
+			HttpClient Client = new DefaultHttpClient();
+			String URL = "http://finance.yahoo.com/d/quotes.csv?f=l1&s="
+            + params[0];
+            
+			try {
+                
+				HttpGet httpget = new HttpGet(URL);
+				ResponseHandler<String> responseHandler = new BasicResponseHandler();
+				String response = Client.execute(httpget, responseHandler);
+				System.out.println("response" + response);
+                
+				return response;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+        
+		/**
+		 * The system calls this to perform work in the UI thread and delivers
+		 * the result from doInBackground()
+		 */
+		protected void onPostExecute(String result) {
+			// Toast is used to display a pop up in android.
+			// Toast toast = Toast.makeText(getApplicationContext(), result,
+			// Toast.LENGTH_SHORT);
+			// toast.show();
+            
+			// Update the stock on the UI.
+			// EditText stockText= (EditText) findViewById(R.id.edit_message);
+			// stockText.setText(result);
+			TextView stockValue = (TextView) findViewById(R.id.stock_value);
+			stockValue.setText("Stock Value:" + result);
+		}
 	}
 }
